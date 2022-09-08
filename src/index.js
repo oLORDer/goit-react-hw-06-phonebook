b41c9a7f5a6a6d7847b6ba0a93b1f29d27c49ff2
oLORDer <justsendmeow@gmail.com>
2022-09-08 23:14:30 +0300

    localStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from 'redux/store';
+import store, { persister } from 'redux/store';
 
 import App from 'components/App';
 import './index.css';
@@ -10,7 +11,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persister}>
+      <App />
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
