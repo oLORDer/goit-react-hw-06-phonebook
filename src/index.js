a97b96bee324ccaddb496b81678c02be62dee8e1
oLORDer <justsendmeow@gmail.com>
2022-09-09 11:18:50 +0300

    localStor/whiteLiist

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persister}>
-      <App />
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -19,6 +19,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
+  whiteList: ['contacts'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
